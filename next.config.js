84c77a1211fcc78aded34778e783df02e1b5afa8
Jimi Ahlgren <jimi.ahlgren@gmail.com>
2024-01-10 11:47:24 +0200

    ignore lint during build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n
+  i18n,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
